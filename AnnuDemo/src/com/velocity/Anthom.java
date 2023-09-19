c58db1d4667d5bd530e757b1ba31669bab17b0c0
Uday3421github <udaysonawane42@gmail.com>
2023-09-19 18:20:57 +0530

    Anthom added to sop

---
+++ AnnuDemo/src/com/velocity/Anthom.java
@@ -0,0 +1,48 @@
+package com.velocity;
+
+public class Anthom {
+
+public static void main(String[] args) {
+	System.out.println("By His stripes, we are healed\r\n" + 
+			"By His nail-pierced hands, we're free\r\n" + 
+			"By His blood, we're washed clean\r\n" + 
+			"Now we have the victory\r\n" + 
+			"The power of sin is broken\r\n" + 
+			"Jesus overcame it all\r\n" + 
+			"He has won our freedom\r\n" + 
+			"Jesus has won it all\r\n" + 
+			"Hallelujah, You have won the victory\r\n" + 
+			"Hallelujah, You have won it all for me\r\n" + 
+			"Death could not hold You down, You are the risen King\r\n" + 
+			"Seated in majesty, You are the risen King\r\n" + 
+			"By His stripes, we are healed\r\n" + 
+			"By His nail-pierced hands, we're free\r\n" + 
+			"By His blood, we're washed clean\r\n" + 
+			"Now we have the victory\r\n" + 
+			"The power of sin is broken\r\n" + 
+			"Jesus overcame it all\r\n" + 
+			"He has won our freedom\r\n" + 
+			"Jesus has won it all\r\n" + 
+			"Hallelujah, You have won the victory\r\n" + 
+			"Hallelujah, You have won it all for me\r\n" + 
+			"Death could not hold You down, You are the risen King\r\n" + 
+			"Seated in majesty, You are the risen King\r\n" + 
+			"Our God is risen, He is alive\r\n" + 
+			"He won the victory, He reigns on high\r\n" + 
+			"Our God is risen, He is alive\r\n" + 
+			"He won the victory, He reigns on high\r\n" + 
+			"Our God is risen, He is alive\r\n" + 
+			"He won the victory, He reigns on high\r\n" + 
+			"Our God is risen, He is alive\r\n" + 
+			"He won the victory, He reigns on high\r\n" + 
+			"Hallelujah, You have won the victory\r\n" + 
+			"Hallelujah, You have won it all for me\r\n" + 
+			"Death could not hold You down, You are the risen King\r\n" + 
+			"Seated in majesty, You are the risen King\r\n" + 
+			"Hallelujah, You have won the victory\r\n" + 
+			"Hallelujah, You have won it all for me\r\n" + 
+			"Death could not hold You down, You are the risen King\r\n" + 
+			"Seated in majesty, You are the risen King\r\n" + 
+			"");
+}
+}
