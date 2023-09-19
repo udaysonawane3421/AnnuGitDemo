4beefefa16ad4110f88abe911a0faf57273685d6
Uday3421github <udaysonawane42@gmail.com>
2023-09-19 17:25:45 +0530

    This is third commit

--- AnnuDemo/src/com/velocity/Sample.java
+++ AnnuDemo/src/com/velocity/Sample.java
@@ -5,5 +5,9 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("I am Annu...");
 	System.out.println("I am a Lawyer..");
+	
+	for(int i=0;i<=10;i++) {
+		System.out.println(i);
+	}
 }
 }
---
+++ AnnuDemo/src/com/velocity/Sample2.java
@@ -0,0 +1,8 @@
+package com.velocity;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		System.out.println("I am goldy");
+	}
+}
